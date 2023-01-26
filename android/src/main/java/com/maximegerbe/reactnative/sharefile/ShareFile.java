13d384faa3de46e54740344e20356c2565c34435
Firas Bouadila <92023947+firasbou@users.noreply.github.com>
2023-01-26 18:10:40 +0100

    Fix build failing on android

--- android/src/main/java/com/maximegerbe/reactnative/sharefile/ShareFile.java
+++ android/src/main/java/com/maximegerbe/reactnative/sharefile/ShareFile.java
@@ -8,7 +8,7 @@ package com.maximegerbe.reactnative.sharefile;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.util.Base64;
 import android.app.Activity;
 
